09f352650dddf9d521667f830cf8681c0dff0edf
Jerome Ghartey <gharteyjerome@gmail.com>
2023-07-31 15:02:17 +0100

    added features and download sections

--- src/App.js
+++ src/App.js
@@ -25,16 +25,19 @@ const App = () => {
         mockupImg={assets.feature}
         reverse
       />
-       <SectionWrapper 
+      <SectionWrapper
         title="Creative way to showcase the store"
         description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
         mockupImg={assets.mockup}
         banner="banner02"
       />
 
-<div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
-        <p className={`${styles.pText} ${styles.whiteText}`}> &copy; 2023 {" "}
-        <span className="bold">ProNef Marketplace</span>
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          {" "}
+          &copy; 2023 <span className="bold">ProNef Marketplace</span>
         </p>
       </div>
     </>
